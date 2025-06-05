74dfddcbe1a903c79f3d2af301f69461e67907f2
ocjarman <ocjarman@gmail.com>
2025-06-05 12:22:49 -0400

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Work from './components/Work'
 import About from './components/About'
-import PageWrapper from './components/PageWrapper'
 import ContactForm from './components/ContactForm'
 import HomeHero from './components/HomeHero'
 import ProjectsPage from './components/ProjectsPage'
 
 function Home() {
-  return (
-    <PageWrapper>
-      <HomeHero />
-    </PageWrapper>
-  )
+  return <HomeHero />
 }
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio/">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
